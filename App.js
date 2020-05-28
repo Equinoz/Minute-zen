9c319fbf2f4170469e7cbac0a858b78c42bd62cf
Equinoz <julien.lecoz33@gmail.com>
2020-05-28 19:18:15 +0200

    Persistance des données

--- App.js
+++ App.js
@@ -4,14 +4,20 @@ import SplashScreen from "react-native-splash-screen";
 import Navigator from "./Navigation/Navigator";
 import { Provider } from "react-redux";
 import Store from "./Store/configureStore";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/es/integration/react";
 
 export default function App() {
 	useEffect(() => {
 		SplashScreen.hide();
-	}, []);
+  }, []);
+
+  let persistor = persistStore(Store);
   return (
     <Provider store={ Store } >
-      <Navigator />
+      <PersistGate persistor={ persistor }>
+        <Navigator />
+      </PersistGate>
     </Provider>
   );
 };
